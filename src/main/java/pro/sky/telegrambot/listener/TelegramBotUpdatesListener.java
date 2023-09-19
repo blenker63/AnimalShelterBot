fed8de2855e956d07227f9c5004ad16431637c00
vaceslavkameronov <slka@mail.ru>
2023-09-19 22:18:45 +0400

    Сделано приветствие со стороны бота

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
+            if (update.message().text().equals("/start")) {
+                SendMessage text = new SendMessage(update.message().chat().id(),
+                        "Привет " + update.message().from().firstName() + "! Вас приветствует приют для животных КотоПёс!");
+                SendResponse sendResponse = telegramBot.execute(text);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
