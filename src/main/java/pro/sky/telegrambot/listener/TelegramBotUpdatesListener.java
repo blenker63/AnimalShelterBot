a03c07ace7fd95d998b3e5cc261b49d54e0299ed
LeonidHome <defenser2205@gmail.com>
2023-09-26 19:14:48 +0300

    добавил чтобы  кнопки были  две

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,14 +3,18 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.service.KeyboardService;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,6 +24,8 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Autowired
     private TelegramBot telegramBot;
+    @Autowired
+    KeyboardService keyboardService;
 
     @PostConstruct
     public void init() {
@@ -35,6 +41,14 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                 SendMessage text = new SendMessage(update.message().chat().id(),
                         "Привет " + update.message().from().firstName() + "! Вас приветствует приют для животных КотоПёс!");
                 SendResponse sendResponse = telegramBot.execute(text);
+                SendMessage message = new SendMessage(update.message().chat().id(), "выберите приют который вам необходим")
+                        .replyMarkup(keyboardService.shelterSelectionMenu());
+                SendResponse sendResponse1 = telegramBot.execute(message);
+
+
+
+
+
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
