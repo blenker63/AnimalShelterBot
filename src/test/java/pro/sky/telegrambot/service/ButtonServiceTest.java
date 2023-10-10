678425134508f43f05a293d079e86d77a653305d
LeonidHome <defenser2205@gmail.com>
2023-10-11 00:56:57 +0300

    тест для кнопки

---
+++ src/test/java/pro/sky/telegrambot/service/ButtonServiceTest.java
@@ -0,0 +1,73 @@
+package pro.sky.telegrambot.service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import pro.sky.telegrambot.repository.*;
+
+import static java.lang.System.out;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ButtonServiceTest {
+    @Mock
+    private R_Animal rAnimalMock;
+    @Mock
+    private R_Shelter rShelterMock;
+    @Mock
+    private R_Photo rPhoto;
+    private ButtonService service;
+
+    @BeforeEach
+    void initService() {
+        service = new ButtonService(rPhoto, rAnimalMock, rShelterMock);
+    }
+
+    @Test
+    void setButtonStartMenuTest() {
+        Long chatId = 152152L;
+        String text = "test";
+        SendMessage test = service.setButtonStartMenu(chatId, text);
+        assertThat(test).isNotNull().isInstanceOf(SendMessage.class);
+
+    }
+
+    @Test
+    void setButtonDogShelterInfo() {
+    }
+
+    @Test
+    void setButtonCatShelterInfo() {
+    }
+
+    @Test
+    void setButtonLooKAnimal() {
+    }
+
+    @Test
+    void choiceAnimal() {
+    }
+
+    @Test
+    void countCat() {
+    }
+
+    @Test
+    void countDog() {
+    }
+
+    @Test
+    void findCallBackByAnimal() {
+    }
+
+    @Test
+    void findPhoto() {
+    }
+}
