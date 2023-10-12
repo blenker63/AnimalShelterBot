655e4b5f3e729b80ad54ecba04e3dd1924471e62
LeonidHome <defenser2205@gmail.com>
2023-10-13 00:49:48 +0300

    тесты для кнопок

--- src/test/java/pro/sky/telegrambot/service/ButtonServiceTest.java
+++ src/test/java/pro/sky/telegrambot/service/ButtonServiceTest.java
@@ -6,14 +6,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import pro.sky.telegrambot.model.Animal;
+import pro.sky.telegrambot.model.AnimalOwner;
+import pro.sky.telegrambot.model.Photo;
 import pro.sky.telegrambot.repository.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static java.lang.System.out;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ButtonServiceTest {
@@ -31,7 +39,6 @@ class ButtonServiceTest {
     }
 
 
-
     @Test
     void setButtonStartMenuTest() {
         Long chatId = 152152L;
@@ -42,7 +49,7 @@ class ButtonServiceTest {
     }
 
     @Test
-    void setButtonDogShelterInfo() {
+    void setButtonDogShelterInfoTest() {
         Long chatId = 152152L;
         String text = "test";
         SendMessage test = service.setButtonDogShelterInfo(chatId, text);
@@ -55,30 +62,84 @@ class ButtonServiceTest {
     }
 
     @Test
-    void setButtonCatShelterInfo() {
+    void setButtonCatShelterInfoTest() {
+        Long chatId = 152152L;
+        String text = "test";
+        SendMessage test = service.setButtonCatShelterInfo(chatId, text);
+        SendMessage expected = new SendMessage();
+        expected.setChatId(152152L);
+        expected.setText("test");
+        assertThat(test).isNotNull().isInstanceOf(SendMessage.class);
+        assertThat(test.getChatId()).isEqualTo(expected.getChatId());
+        assertThat(test.getText()).isEqualTo(expected.getText());
     }
 
     @Test
-    void setButtonLooKAnimal() {
+    void setButtonLooKAnimalTest() {
+        Long chatId = 152152L;
+        String text = "test";
+        String animalId = "testanimal";
+        SendMessage test = service.setButtonLooKAnimal(chatId, animalId, text);
+        assertThat(test).isNotNull().isInstanceOf(SendMessage.class);
+        SendMessage expected = new SendMessage();
+        expected.setChatId(chatId);
+        expected.setText(text);
+        assertThat(test.getChatId()).isEqualTo(expected.getChatId());
+        assertThat(test.getText()).isEqualTo("\t" + expected.getText());
     }
 
     @Test
-    void choiceAnimal() {
+    void choiceAnimalTest() {
+        Long chatId = 152152L;
+        String text = "test";
+        String animalId = "testanimal";
+        SendMessage test = service.choiceAnimal(chatId, animalId, text);
+        assertThat(test).isNotNull().isInstanceOf(SendMessage.class);
+        SendMessage expected = new SendMessage();
+        expected.setChatId(chatId);
+        expected.setText(text);
+        assertThat(test.getChatId()).isEqualTo(expected.getChatId());
+        assertThat(test.getText()).isEqualTo("    " + expected.getText());
     }
 
     @Test
-    void countCat() {
+    void countCatTest() {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("кот", "will", 3, "maincoon"));
+        animals.add(new Animal("кот", "will1", 3, "maincoon"));
+        animals.add(new Animal("собака", "bill", 2, "colli"));
+        String type = "кот";
+        when(rAnimalMock.findAnimalByAnimalTypeIgnoreCase(type)).thenReturn(animals.subList(0, 2));
+        assertThat(service.countCat().size()).isEqualTo(2);
+        verify(rAnimalMock, times(1)).findAnimalByAnimalTypeIgnoreCase(type);
     }
 
     @Test
-    void countDog() {
+    void countDogTest() {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("кот", "will", 3, "maincoon"));
+        animals.add(new Animal("кот", "will1", 3, "maincoon"));
+        animals.add(new Animal("собака", "bill", 2, "colli"));
+        String type = "собака";
+        when(rAnimalMock.findAnimalByAnimalTypeIgnoreCase(type)).thenReturn(animals.subList(2, 3));
+        assertThat(service.countDog().size()).isEqualTo(1);
+        verify(rAnimalMock, times(1)).findAnimalByAnimalTypeIgnoreCase(type);
     }
 
     @Test
-    void findCallBackByAnimal() {
+    void findCallBackByAnimalTest() {
+        Animal animal = new Animal("кот", "will", 3, "maincoon");
+        when(rAnimalMock.findAnimalById(1L)).thenReturn(animal);
+        assertThat(service.findCallBackByAnimal(1L).getName()).isEqualTo("will");
+        verify(rAnimalMock, times(1)).findAnimalById(1L);
+
     }
 
     @Test
-    void findPhoto() {
+    void findPhotoTest() {
+        //byte[] data = new byte[1024];
+        //Photo photo = new Photo("filepath", 1024L, "bng", data);
+       //when(rPhoto.findPhotoByAnimalId(1L)).thenReturn(photo);
+        verify(rPhoto, times(1)).findPhotoByAnimalId(1L);
     }
 }
