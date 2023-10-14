5e9796e15da0b9740b19360dc1cb37c9a3518145
vaceslavkameronov <slka@mail.ru>
2023-10-14 15:44:37 +0400

    перенесено из FillingDataBaseController в соответствующие контроллеры

--- src/main/java/pro/sky/telegrambot/controller/AnimalOwnerController.java
+++ src/main/java/pro/sky/telegrambot/controller/AnimalOwnerController.java
@@ -1,4 +1,43 @@
 package pro.sky.telegrambot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import pro.sky.telegrambot.model.AnimalOwner;
+import pro.sky.telegrambot.service.AddService;
+
+/**
+ * класс содержит эндпойнты для внесения информации в базу данных по взявшим животных из приюта
+ */
 public class AnimalOwnerController {
+    private final AddService addService;
+
+
+    public AnimalOwnerController(AddService addService) {
+        this.addService = addService;
+    }
+
+    @Operation(
+            summary = "внесение информации по владельцу взявшего животное из приюта",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Данные по владельцу внесены",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = AnimalOwner.class))
+                            )
+                    )
+            }
+    )
+    @PostMapping("/animal-owner/{name}/{phoneNumber}/{eMail}/{trialPeriod}")
+    public void AnimalOwnerSave(@PathVariable String name, @PathVariable String phoneNumber, @PathVariable String eMail, @PathVariable boolean trialPeriod) {
+        addService.AnimalOwnerSave(name, phoneNumber, eMail, trialPeriod);
+
+    }
 }
